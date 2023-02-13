df03da1e6108008bcdc131a5e0cfab42ff30d43a
Eelaaf Zahid <eelaaf.zahid@leidos.com>
2023-02-12 23:39:14 -0500

    completed basic app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 
 # Instantiate our app
 app = Flask(__name__)
@@ -13,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Creating the database
 db = SQLAlchemy(app)
 
+
 # Creating a model class for the todo items with three entries
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,14 +26,50 @@ class Todo(db.Model):
 # Homepage will list all the todos
 @app.route('/')
 def home():
-# List of todos pulled from database
+    # List of todos pulled from database
     todo_list = Todo.query.all()
-# HTML rendered of todo list
+    # HTML rendered of todo list
     return render_template("base.html", todo_list=todo_list)
 
+# Route to add new todo
+@app.route("/add", methods=["POST"])
+def add():
+    # New todo is created 
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    # Database is updated
+    db.session.add(new_todo)
+    db.session.commit()
+    # Redirect back to homepage
+    return redirect(url_for("home"))
+
+# Route to update a certain todo's completion
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # Todos are filtered by given id
+    todo = Todo.query.filter_by(id=todo_id).first()
+    # That todo's completion is toggled
+    todo.complete = not todo.complte
+    # Database is updated
+    db.session.commit()
+    # Redirect back to homepage
+    return redirect(url_for("home"))
+
+# Route to delete a todo
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # Todo to be deleted is found by filtering for id
+    todo = Todo.query.filter_by(id=todo_id).first()
+    # That todo is deleted from the database
+    db.session.delete(todo)
+    # Database is updated
+    db.session.commit()
+    # Redirect back to homepage
+    return redirect(url_for("home"))
+
 if __name__ == "__main__":
-# Creates database and tables
+    # Creates database and tables
     db.create_all()
-# debug=True means server doesn't reload for changes in code
+    # debug=True means server doesn't reload for changes in code
     app.run(debug=True)
 
